58f52ca90aac55929fa4453b3a49523e13e2af49
zhaoyanglin <zhaoyanglin@msn.com>
2019-03-03 23:49:08 -0600

    added the if

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -6,20 +6,25 @@ import { Link } from "react-router-dom";
 class Review extends Component {
 
     addToData = () => {
-        Axios({
-            method: 'POST',
-            url: '/feedBack',
-            data: {
-                feeling: this.props.mainReducer.feeling,
-                understanding: this.props.mainReducer.understanding,
-                support: this.props.mainReducer.support,
-                comments: this.props.mainReducer.comments,
-            }
-        }).then((response) => {
-            console.log(response);
-        })
-    }
 
+        if (this.props.mainReducer.feeling === '' || this.props.mainReducer.understanding === '' || this.props.mainReducer.support === '' || this.props.mainReducer.comments === '') {
+           return alert('input incomplete')
+        } else {
+            Axios({
+                method: 'POST',
+                url: '/feedBack',
+                data: {
+                    feeling: this.props.mainReducer.feeling,
+                    understanding: this.props.mainReducer.understanding,
+                    support: this.props.mainReducer.support,
+                    comments: this.props.mainReducer.comments,
+                }
+            }).then((response) => {
+                console.log(response);
+                this.props.history.push('/lastPage')
+            })
+        }
+    }
     render() {
 
         return (
@@ -31,7 +36,7 @@ class Review extends Component {
                     <p>Support:{this.props.mainReducer.support}</p>
                     <p>Comments:{this.props.mainReducer.comments}</p>
 
-                    <Link to='/lastPage' onClick={this.addToData}>Submit</Link>
+                    <button onClick={this.addToData}>Submit</button>
 
                 </ul>
             </div>
